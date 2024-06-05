8ad3fa8c817f38d47ea0ed81c874ca0ea190ff65
SergeyD <min3drav1@gmail.com>
2024-06-05 14:33:36 +0900

    Fix send line trace from camera center

--- Source/AreYouLost/Private/Components/InteractionComponent.cpp
+++ Source/AreYouLost/Private/Components/InteractionComponent.cpp
@@ -6,7 +6,8 @@ UInteractionComponent::UInteractionComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
-void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                          FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
@@ -17,17 +18,29 @@ void UInteractionComponent::Interact() const
 	check(Owner);
 	FVector StartPosition;
 	FRotator LookRotator;
-	Owner->GetActorEyesViewPoint(StartPosition, LookRotator);
+
+	if(APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+	{
+		StartPosition = PlayerController->PlayerCameraManager->GetCameraLocation();
+		LookRotator = PlayerController->PlayerCameraManager->GetCameraRotation();
+	}
+	else
+	{
+		Owner->GetActorEyesViewPoint(StartPosition, LookRotator);
+	}
+
 	FVector EndPosition = StartPosition + LookRotator.Vector() * Distance;
 
 	FCollisionQueryParams CollisionQueryParams;
 	CollisionQueryParams.AddIgnoredActor(Owner);
 	FCollisionResponseParams CollisionResponseParams;
 	FHitResult HitResult;
-	bool IsHit = GetWorld()->LineTraceSingleByChannel(HitResult, StartPosition, EndPosition, ECollisionChannel::ECC_Visibility, CollisionQueryParams, CollisionResponseParams);
-	
+	bool IsHit = GetWorld()->LineTraceSingleByChannel(HitResult, StartPosition, EndPosition,
+	                                                  ECollisionChannel::ECC_Visibility, CollisionQueryParams,
+	                                                  CollisionResponseParams);
+
 	AActor* HitActor = HitResult.GetActor();
-	if(IsHit && HitActor && HitActor->Implements<UInteractableInterface>())
+	if (IsHit && HitActor && HitActor->Implements<UInteractableInterface>())
 	{
 		APawn* Pawn = Cast<APawn>(Owner);
 		IInteractableInterface::Execute_Use(HitActor, Pawn);
@@ -37,6 +50,4 @@ void UInteractionComponent::Interact() const
 	{
 		DrawDebugLine(GetWorld(), HitResult.TraceStart, HitResult.TraceEnd, FColor::Red, false, 2.0f, 0, 4.0f);
 	}
-	
 }
-
